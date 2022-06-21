993c798456e864c764e58cae4a782ac75fdb0439
Dolapo-Mosuro <mosurodolapo@gmail.com>
2022-06-21 18:31:01 +0100

    update secret key

--- I4G0207316NU/I4G0207316NU/settings.py
+++ I4G0207316NU/I4G0207316NU/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for I4G0207316NU project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i0vf_l0sxq)wx6u@hp8h-p55ex3b&6zivd)ulxq#7+&obtr)pt'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
